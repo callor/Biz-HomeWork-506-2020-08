f624cff97d7bf2c100a2a24863a349a41149c120
callor <callor@callor.com>
2020-08-25 09:54:22 +0900

    볼링점수계산

--- Java_HomeWork_004/src/com/callor/game/service/BowllGameService.java
+++ Java_HomeWork_004/src/com/callor/game/service/BowllGameService.java
@@ -20,10 +20,11 @@ public class BowllGameService {
 
 	public int getScore() {
 
-		int totalScore = 0;
+		
 
 		System.out.print(frames.toString());
 		
+		int totalScore = 0;
 		int size = frames.size();
 		for (int thisFrame = 0 ; thisFrame < size - 2; thisFrame ++) {
 			Integer score = frames.get(thisFrame);
@@ -31,20 +32,24 @@ public class BowllGameService {
 			// 현재 frame strike 이면 이후 2번의 frame 점수를 합산한다.
 			// 예)1 frame 이 strike 이면  2, 3 frame 점수 모두 합산
 			if (score == 10) {
-				totalScore += 10; // 현재 frame 10
+//				System.out.printf("ST %d,%d  ",score,frames.get(thisFrame + 1));
+				totalScore += score; // 현재 frame 10
 				totalScore += frames.get(thisFrame + 1); // next 점수합산
 				totalScore += frames.get(thisFrame + 2); // next 점수합산
 				
 				// 현재 frame 점수와 다음 frame 점수합이 10이면
 				// spare 처리가 되었으면
 			} else if(score + frames.get(thisFrame + 1) == 10) {
-				totalScore += 10; // 현재 frame + next frame = 10
-				totalScore += frames.get(thisFrame + 2); 
+				totalScore += score; // 현재 frame + next frame = 10
+				totalScore += frames.get(thisFrame + 1);
+				totalScore += frames.get(thisFrame + 2);
 				thisFrame += 1;
+				
 			} else {
+//				System.out.printf("ELSE %d,%d  ",score,frames.get(thisFrame + 1));
 				totalScore += score;
-				totalScore += frames.get(thisFrame+1);
-				thisFrame += 1;
+				totalScore += frames.get(thisFrame + 1);
+				thisFrame++;
 			}
 		}
 		return totalScore;
